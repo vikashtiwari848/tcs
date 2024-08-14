eff92e8d7e323cbf632f21339a0471a5793aa5c7
vikashtiwari8481 <vikashtiwariwork@gmail.com>
2024-08-14 11:56:16 +0530

    commit2

--- component/Home.js
+++ component/Home.js
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 import React, { useEffect, useState } from "react";
 
@@ -12,9 +13,11 @@ import {
   Platform,
   Alert
 } from "react-native";
+import { API_URL } from "../Api";
 
 
 const HomeScreen = ({navigation}) => {
+    const [balance,setBalance]=useState(0);
    const logOut=async()=>{
     try {
         await AsyncStorage.removeItem('accountNumber').then(()=>{
@@ -25,10 +28,26 @@ const HomeScreen = ({navigation}) => {
         console.log(exception)
     }
    }
+
+   useEffect(()=>{
+    const unsubscribe = navigation.addListener('focus', async() => {
+        let value=await AsyncStorage.getItem('accountNumber');
+        value=JSON.parse(value);
+        await axios.post(`${API_URL}/getBalance`,{accountNumber:value
+                }).then(res=>{
+                    setBalance(res?.data?.balance??0);
+                    console.log("success",res.data)                
+                }).catch(err=>{
+                  Alert.alert(err?.response?.data?.message ??"Internal Server Error")
+                    console.log(err)})
+      });
+     return unsubscribe;  
+   },[navigation])
   
     return (
        <View style={styles.container}>
         <Text style={styles.header}>Welcome to SBI</Text>
+        <Text style={styles.balance}>Your Bank Balance {balance}</Text>
         <Button
         title="Transfer Money"
         onPress={() =>
@@ -57,7 +76,13 @@ const HomeScreen = ({navigation}) => {
       marginLeft:80,
       fontWeight: 'bold',
       fontSize: 30,
-    }
+    },
+    balance: {
+       
+        marginLeft:80,
+        fontWeight: 'bold',
+        fontSize: 20,
+      }
  
   });
   export default HomeScreen;
